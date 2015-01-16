d330f1550f4f00d9a05d6524013184bef71777a2
王源果 <g4idrijs@gmail.com>
2015-01-17 03:42:26 +0800

    initial post

---
+++ 6.c
@@ -0,0 +1,12 @@
+// 题目：用*号输出字母C的图案。
+// 1.程序分析：可先用'*'号在纸上写出字母C，再分行输出。
+// 2.程序源代码：
+#include "stdio.h"
+	int main(){
+		printf("Hello C-world!\n");
+		printf(" ****\n");
+		printf(" *\n");
+		printf(" * \n");
+		printf(" ****\n");
+		return 0;
+	}
