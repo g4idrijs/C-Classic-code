48ef2b7b4f6a104daa76ec02dfc3d14b20546676
王源果 <g4idrijs@gmail.com>
2015-01-23 23:48:53 +0800

    initial post

---
+++ 75.c
@@ -0,0 +1,21 @@
+//题目：放松一下，算一道简单的题目。
+//1.程序分析：
+//2.程序源代码：
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	int i,n;
+	for(i=1;i<5;i++){
+		n=0;
+		if(i!=1)
+			n=n+1;
+		if(i==3)
+			n=n+1;
+		if(i==4)
+			n=n+1;
+		if(i!=4)
+			n=n+1;
+		if(n==3)
+			printf("zhu hao shi de shi:%c",64+i);
+	　}
+}
