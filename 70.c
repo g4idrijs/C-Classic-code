4f362e2e2f2a83c1b34c94e0a93f1d1f3764539d
王源果 <g4idrijs@gmail.com>
2015-01-23 23:31:27 +0800

    initial post

---
+++ 70.c
@@ -0,0 +1,26 @@
+//题目：写一个函数，求一个字符串的长度，
+//	在main函数中输入字符串，并输出其长度。　　　
+//1.程序分析：
+//2.程序源代码：
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	int len;
+	char *str[20];
+	printf("please input a string:\n");
+	scanf("%s",str);
+	len=length(str);
+	printf("the string has %d characters.",len);
+}
+
+length(p)
+char *p;
+{
+	int n;
+	n=0;
+	while(*p!='\0'){
+		n++;
+		p++;
+	}
+	return n;
+}
